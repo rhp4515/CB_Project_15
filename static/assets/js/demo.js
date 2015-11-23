9a483dba861ec0eb779f9cd67e2bfb2d042c5086
Arvind Ram <arvindram03@gmail.com>
2015-11-22 20:50:27 -0500

    Cleaning up UI and making sidebar collapsible

--- static/assets/js/demo.js
+++ static/assets/js/demo.js
@@ -2,23 +2,65 @@ type = ['','info','success','warning','danger'];
     	
 
 demo = {
-    initPickColor: function(){
-        $('.pick-class-label').click(function(){
-            var new_class = $(this).attr('new-class');  
-            var old_class = $('#display-buttons').attr('data-class');
-            var display_div = $('#display-buttons');
-            if(display_div.length) {
-            var display_buttons = display_div.find('.btn');
-            display_buttons.removeClass(old_class);
-            display_buttons.addClass(new_class);
-            display_div.attr('data-class', new_class);
-            }
-        });
-    },
-
     initBoxPlot: function(box_data) {
       //var obs_data = JSON.parse(box_data)
       $('#chartHours').highcharts({
+          chart: {
+              type: 'boxplot'
+          },
+          title: {
+              text: 'Highcharts Box Plot Example'
+          },
+          legend: {
+              enabled: true
+          },
+          xAxis: {
+              categories: ['1', '2', '3', '4', '5'],
+              title: {
+                  text: 'Experiment No.'
+              }
+          },
+          yAxis: {
+              title: {
+                  text: 'Observations'
+              },
+              plotLines: [{
+                  value: 932,
+                  color: 'red',
+                  width: 1,
+                  label: {
+                      text: 'Theoretical mean: 932',
+                      align: 'center',
+                      style: {
+                          color: 'gray'
+                      }
+                  }
+              }]
+          },
+          series: [{
+              name: 'Observations',
+              data: box_data.data,
+              tooltip: {
+                  headerFormat: '<em>Experiment No {point.key}</em><br/>'
+              }
+          }, {
+              name: 'Outlier',
+              color: Highcharts.getOptions().colors[0],
+              type: 'scatter',
+              data: box_data.outlier_data,
+              marker: {
+                  fillColor: 'white',
+                  lineWidth: 1,
+                  lineColor: Highcharts.getOptions().colors[0]
+              },
+              tooltip: {
+                  pointFormat: 'Observation: {point.y}'
+              }
+          }]
+
+      });
+
+      $('#chartHours1').highcharts({
           chart: {
               type: 'boxplot'
           },
@@ -135,123 +177,65 @@ demo = {
             data: [1, 3, 4, 3, 3, 5, 4]
         }]
     });
-    },
-    
-    initChartist: function(){
-        
-        // var dataSales = {
-        //   labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
-        //   series: [
-        //      [287, 385, 490, 492, 554, 586, 698, 695, 752, 788, 846, 944],
-        //     [67, 152, 143, 240, 287, 335, 435, 437, 539, 542, 544, 647],
-        //     [23, 113, 67, 108, 190, 239, 307, 308, 439, 410, 410, 509]
-        //   ]
-        // };
-        
-        // var optionsSales = {
-        //   lineSmooth: false,
-        //   low: 0,
-        //   high: 800,
-        //   showArea: true,
-        //   height: "245px",
-        //   axisX: {
-        //     showGrid: false,
-        //   },
-        //   lineSmooth: Chartist.Interpolation.simple({
-        //     divisor: 3
-        //   }),
-        //   showLine: false,
-        //   showPoint: false,
-        // };
-        
-        // var responsiveSales = [
-        //   ['screen and (max-width: 640px)', {
-        //     axisX: {
-        //       labelInterpolationFnc: function (value) {
-        //         return value[0];
-        //       }
-        //     }
-        //   }]
-        // ];
-    
-        // Chartist.Line('#chartHours', dataSales, optionsSales, responsiveSales);
-        
-    
-        var data = {
-          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-          series: [
-            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-            [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
-          ]
-        };
-        
-        var options = {
-            seriesBarDistance: 10,
-            axisX: {
-                showGrid: false
-            },
-            height: "245px"
-        };
-        
-        var responsiveOptions = [
-          ['screen and (max-width: 640px)', {
-            seriesBarDistance: 5,
-            axisX: {
-              labelInterpolationFnc: function (value) {
-                return value[0];
-              }
+    $('#chartPreferences1').highcharts({
+        chart: {
+            type: 'areaspline'
+        },
+        title: {
+            text: 'Average fruit consumption during one week'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'left',
+            verticalAlign: 'top',
+            x: 150,
+            y: 100,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+        },
+        xAxis: {
+            categories: [
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday',
+                'Sunday'
+            ],
+            plotBands: [{ // visualize the weekend
+                from: 4.5,
+                to: 6.5,
+                color: 'rgba(68, 170, 213, .2)'
+            }]
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit units'
             }
-          }]
-        ];
-        
-        Chartist.Bar('#chartActivity', data, options, responsiveOptions);
-
-    
-        /*var dataPreferences = {
-            series: [
-                [25, 30, 20, 25]
-            ]
-        };
-        
-        var optionsPreferences = {
-            donut: true,
-            donutWidth: 40,
-            startAngle: 0,
-            total: 100,
-            showLabel: false,
-            axisX: {
-                showGrid: false
+        },
+        tooltip: {
+            shared: true,
+            valueSuffix: ' units'
+        },
+        credits: {
+            enabled: false
+        },
+        plotOptions: {
+            areaspline: {
+                fillOpacity: 0.5
             }
-        };
-    
-        Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
-        
-        Chartist.Pie('#chartPreferences', {
-          labels: ['62%','32%','6%'],
-          series: [62, 32, 6]
-        });  */ 
+        },
+        series: [{
+            name: 'John',
+            data: [3, 4, 3, 5, 4, 10, 12]
+        }, {
+            name: 'Jane',
+            data: [1, 3, 4, 3, 3, 5, 4]
+        }]
+    });
     },
-    
-    initGoogleMaps: function(){
-        var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
-        var mapOptions = {
-          zoom: 13,
-          center: myLatlng,
-          scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-          styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
-    
-        }
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-        
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            title:"Hello World!"
-        });
-        
-        // To add the marker to the map, call setMap();
-        marker.setMap(map);
-    },
-    
   	showNotification: function(from, align){
       	color = Math.floor((Math.random() * 4) + 1);
       	
