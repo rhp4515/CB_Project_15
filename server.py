8ee954585ad8651d1e6426db5918c1c87ed2d2d7
Hari Prasath Raman <hraman@cs.stonybrook.edu>
2015-12-13 23:26:47 -0500

    removing not used function in server.py

--- server.py
+++ server.py
@@ -7,26 +7,6 @@ import sys, traceback
 
 app = Flask(__name__, static_url_path='/static')
 
-usr_obj = {}
-
-data = [[[760, 801, 848, 895, 965], [733, 853, 939, 980, 1080], [714, 762, 817, 870, 918], [724, 802, 806, 871, 950]],
-		[[12, 20, 34, 10, 23], [34, 24, 12, 45, 34], [13, 46, 2, 3, 6], [33, 43, 10, 19, 18]],
-        [[67, 45, 78, 67, 68], [95, 54, 45, 68, 78], [13, 46, 2, 3, 6], [33, 43, 10, 19, 18]]]
-
-@app.route('/user')
-def users():
-	usr_obj["uid"] = request.args.get('id')
-	usr_obj["box"] = {}
-	usr_obj["box"]["data"] = data[random.randint(0, 2)]
-
-	usr_obj["box"]["outlier_data"] = [
-										[0, 644],
-										[4, 718],
-										[4, 951],
-										[4, 969]
-									]
-	return json.dumps(usr_obj)
-
 def get_corr_metrics(table_name, offset):
 	try:
 		conn = pg8000.connect(user="postgres", password="forgotpassword", database="cb15rna", host="cb15rna.ciacashmbpf0.us-east-1.rds.amazonaws.com")
@@ -66,6 +46,8 @@ def get_metrics(table_name, offset):
 			
 		metricDict['gcTPM'] = gcTPM
 		metricDict['lenTPM'] = lenTPM	
+		print "length of gcTPM", len(gcTPM)
+		print "length of lenTPM", len(lenTPM)
 		return metricDict
 	except:  
 		traceback.print_exc()
